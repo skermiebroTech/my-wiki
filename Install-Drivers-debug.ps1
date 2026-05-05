2b40a371263e60115e3ad5ae27df9a8a3464a03e
Skermiebro Tech Tips <67085932+skermiebroTech@users.noreply.github.com>
2026-05-06 09:38:04 +1000

    Update Install-Drivers-debug.ps1

--- Install-Drivers-debug.ps1
+++ Install-Drivers-debug.ps1
@@ -1,20 +1,15 @@
 $xml = "$env:TEMP\CatalogPC.xml"
 [xml]$cat = [System.IO.File]::ReadAllText($xml).TrimStart([char]0xFEFF)
 
-# How many SoftwareComponent nodes does XPath actually find?
-$all = $cat.SelectNodes("//*[local-name()='SoftwareComponent']")
-Write-Host "Total SoftwareComponent nodes: $($all.Count)"
-
-# How many have 'driver pack' in the name?
-$packs = $all | Where-Object {
-    try { $_.SelectSingleNode("*[local-name()='Name']/*[local-name()='Display']").InnerText -match "(?i)driver\s*pack" } catch { $false }
-}
-Write-Host "Driver pack nodes: $($packs.Count)"
-
-# Of those, which ones have a Model with systemID 0B0B?
-$packs | ForEach-Object {
+# Find all nodes for 0B0B and show name + path
+$cat.SelectNodes("//*[local-name()='SoftwareComponent']") | Where-Object {
+    $_.SelectNodes(".//*[local-name()='Model']") | Where-Object { 
+        $_.GetAttribute("systemID") -eq "0B0B" 
+    }
+} | ForEach-Object {
     $name = try { $_.SelectSingleNode("*[local-name()='Name']/*[local-name()='Display']").InnerText } catch { "?" }
-    $ids = $_.SelectNodes(".//*[local-name()='Model']") | ForEach-Object { $_.GetAttribute("systemID") }
-    Write-Host "PACK: $name"
-    Write-Host "  systemIDs: $($ids -join ', ')"
+    $path = $_.GetAttribute("path")
+    Write-Host "NAME: $name"
+    Write-Host "PATH: $path"
+    Write-Host "---"
 }
